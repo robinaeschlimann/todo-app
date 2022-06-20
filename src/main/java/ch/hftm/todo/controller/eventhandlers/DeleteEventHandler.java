c928776a735cffe1a257267a94d7caea1701d23c
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2022-06-20 20:25:46 +0200

    Bestätigungs-Popup für Löschfunktion

--- src/main/java/ch/hftm/todo/controller/eventhandlers/DeleteEventHandler.java
+++ src/main/java/ch/hftm/todo/controller/eventhandlers/DeleteEventHandler.java
@@ -7,6 +7,10 @@ import ch.hftm.todo.service.MessageService;
 import ch.hftm.todo.service.TodoService;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class DeleteEventHandler implements EventHandler<ActionEvent>
 {
@@ -20,10 +24,20 @@ public class DeleteEventHandler implements EventHandler<ActionEvent>
     @Override
     public void handle(ActionEvent event)
     {
-        TodoData todoData = TodoService.getInstance().getTodo( todoId );
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle( "ToDo löschen" );
+        alert.setHeaderText( "ToDo löschen" );
+        alert.setContentText( "Möchtest du dieses ToDo wirklich löschen?" );
 
-        TodoService.getInstance().deleteTodo( todoId );
+        Optional<ButtonType> result = alert.showAndWait();
 
-        MessageService.getInstance().publishMessage( new TodoChangedEvent( todoData, EChangeType.DELETE ) );
+        if ( result.get() == ButtonType.OK )
+        {
+            TodoData todoData = TodoService.getInstance().getTodo(todoId);
+
+            TodoService.getInstance().deleteTodo(todoId);
+
+            MessageService.getInstance().publishMessage(new TodoChangedEvent(todoData, EChangeType.DELETE));
+        }
     }
 }
