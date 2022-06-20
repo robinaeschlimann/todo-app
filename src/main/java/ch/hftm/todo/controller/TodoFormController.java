0f7b6991a049e559f89788fd80254077d5c94d12
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2022-06-20 20:16:46 +0200

    UI-Anpassungen + Validierung

--- src/main/java/ch/hftm/todo/controller/TodoFormController.java
+++ src/main/java/ch/hftm/todo/controller/TodoFormController.java
@@ -9,11 +9,14 @@ import ch.hftm.todo.service.TodoService;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 
 public class TodoFormController implements Initializable
@@ -58,35 +61,93 @@ public class TodoFormController implements Initializable
 
     public void saveTodo()
     {
-        EChangeType changeType = EChangeType.CREATE;
-
-        Object userData = TodoApp.getTodoFormStage().getUserData();
-        TodoData todoData = null;
-
-        if( userData instanceof TodoData ) // Editieren eines ToDos
+        if( isInputValid() )
         {
-            changeType = EChangeType.UPDATE;
-            todoData = (TodoData) userData;
+            EChangeType changeType = EChangeType.CREATE;
+
+            Object userData = TodoApp.getTodoFormStage().getUserData();
+            TodoData todoData = null;
+
+            if ( userData instanceof TodoData ) // Editieren eines ToDos
+            {
+                changeType = EChangeType.UPDATE;
+                todoData = (TodoData) userData;
+            }
+            else // Erstellen eines ToDos
+            {
+                todoData = new TodoData();
+                todoData.setId(TodoService.getInstance().getNextFreeId());
+                todoData.setDone(false);
+            }
+
+            todoData.setName(todoField.getText());
+            todoData.setDeadline(deadlineField.getEditor().getText());
+            todoData.setPerson(personField.getText());
+            todoData.setDescription(descriptionField.getText());
+            todoData.setGroup(1);
+
+            TodoService.getInstance().saveTodo(todoData);
+
+            MessageService.getInstance().publishMessage(new TodoChangedEvent(todoData, changeType));
+
+            resetAndExit();
         }
-        else // Erstellen eines ToDos
-        {
-            todoData = new TodoData();
-            todoData.setId( TodoService.getInstance().getNextFreeId() );
-            todoData.setDone( false );
-        }
-
-        todoData.setName( todoField.getText() );
-        todoData.setDeadline( deadlineField.getEditor().getText() );
-        todoData.setPerson( personField.getText() );
-        todoData.setDescription( descriptionField.getText() );
-        todoData.setGroup( 1 );
-
-        TodoService.getInstance().saveTodo( todoData );
-
-        MessageService.getInstance().publishMessage( new TodoChangedEvent( todoData, changeType ) );
-
-        resetAndExit();
     }
 
+    public boolean isInputValid()
+    {
+        boolean isValid = false;
+
+        String errorMessage = "";
+
+        String todoFieldText = todoField.getText();
+
+        if( todoFieldText == null || todoFieldText.isEmpty() )
+        {
+            errorMessage += "Bezeichnung ist ein Pflichtfeld\n";
+        }
+
+        String deadline = deadlineField.getEditor().getText();
+
+        if( deadline == null || deadline.isEmpty() )
+        {
+            errorMessage += "Deadline ist ein Pflichtfeld\n";
+        }
+        else
+        {
+            SimpleDateFormat dateFormat = new SimpleDateFormat( "dd.MM.yyyy" );
+
+            try
+            {
+                dateFormat.parse( deadline );
+            } catch ( ParseException e )
+            {
+                errorMessage += "Das Datum muss im Format dd.MM.yyyy angegeben werden\n";
+            }
+        }
+
+        String personFieldText = personField.getText();
+
+        if( personFieldText == null || personFieldText.isEmpty() )
+        {
+            errorMessage += "Zuständige Person ist ein Pflichtfeld\n";
+        }
+
+        if( errorMessage.isEmpty() )
+        {
+            isValid = true;
+        }
+        else
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Felder Valdierung");
+            alert.setHeaderText("Bitte korrigiere die ungültigen Felder.");
+            alert.setContentText(errorMessage);
+            alert.showAndWait();
+
+        }
+
+        return isValid;
+    }
 
 }
